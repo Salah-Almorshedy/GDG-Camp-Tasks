2c9b51d4ac7e49d9ff626a6c19d095eab7435a75
Salah Almorshedy <salahalmorshedy.org@gmail.com>
2025-02-06 20:45:11 +0200

    Add task 4

---
+++ task 4/assets/js/script.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector(".btn")
+let p = document.querySelector("p")
+
+
+function createRandomP() {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = "";
+    for (let i = 0; i <= chars.length; i++) {
+            result += chars.charAt(Math.floor(Math.random() * chars.length))
+        }    
+    return result
+}
+
+btn.addEventListener("click" , ()=>{
+    p.innerText = createRandomP()
+})
